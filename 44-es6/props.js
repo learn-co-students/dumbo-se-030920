8ca84fae695c6ebf4ac1f89d08dda05c4080981b
Eric Kim <hyeokjungkim@gmail.com>
2020-05-06 12:08:05 -0400

    adds lecture

--- 44-es6/props.js
+++ 44-es6/props.js
@@ -1,17 +1,21 @@
 // Is there a shorthand way to create properties in an object literal
 // that have the the same names as variables I have in scope?
 
-  const name = "Eric Kim"
-  const age = 11
-  const eating = ["junk", "garbage"]
+// Importance in Mod 4: 🔥🔥
 
-  const eric = {
-    name: name,
-    age: age,
-    eating: eating
-  }
+const name = "Eric Kim"
+const age = 11
+const eating = ["junk", "garbage"]
+const eric = { eating, age, name }
+// console.log(eric);
 
-  console.log(eric);
+
+// const username = evt.target.username.value
+// const password = evt.target.password.value
+// let obj = {username, password}
+// body: JSON.stringify(obj)
+
+
 
 
 
@@ -20,12 +24,16 @@
 // Is there a way to dynamically define (or compute) property names
 // in an object literal e.g. based on the value of a variable?
 
-  const whatIwantToDoMore = "dance";
+// Importance in Mod 4: 🔥🔥🔥
+
+  const whatIwantToDoMore = "walk";
 
   const newYearsResolution = {
     body: "Write code every day",
-    dance: "more"
+    [whatIwantToDoMore]: "more"
   }
 
+newYearsResolution[whatIwantToDoMore] // => "more"
+newYearsResolution.whatIwantToDoMore // => undefined
 
 console.log(newYearsResolution);
